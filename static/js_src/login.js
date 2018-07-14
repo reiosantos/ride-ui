62476e7c19077f0f6c1a9b94977fbc93cc0bea09
reiosantos <ronireiosantos@gmail.com>
2018-07-14 17:51:17 +0300

    modify login module into es6 syntax

---
+++ static/js_src/login.js
@@ -0,0 +1,62 @@
+import {validatePassword} from "./main";
+import {login_service} from "./services/login.service";
+import {PROPERTY_USER, USER_TYPE_DRIVER, USER_TYPE_PASSENGER} from "./variable_constants";
+
+let login = async (form) => {
+
+	form.preventDefault();
+	form  = form.target;
+
+	let username = form.inputUsername;
+	let password = form.inputPassword;
+
+	let error_panel = document.getElementById("loginError");
+	let username_error = document.getElementById("usernameError");
+	let password_error = document.getElementById("passwordError");
+
+	username_error.style.display = "none";
+	password_error.style.display = "none";
+	error_panel.style.display = "none";
+
+	if (username.value.length > 3) {
+		if (validatePassword(password.value)) {
+
+			const data = {
+				username: username.value,
+				password: password.value
+			};
+			let response = await login_service(data);
+
+			if (typeof response === "boolean" && response){
+				let user= JSON.parse(localStorage.getItem(PROPERTY_USER));
+
+				if (!user) {
+					error_panel.innerHTML = "<b>Please clear your browsers cache and login again.. </b>";
+					error_panel.style.display = "block";
+					return false;
+				}
+
+				if (user.user_type === USER_TYPE_PASSENGER) {
+					location.assign("passenger/index.html");
+
+				} else if (user.user_type === USER_TYPE_DRIVER) {
+					location.assign("driver/index.html");
+				}
+				return true;
+			}
+			if (response.hasOwnProperty("error_message")) {
+				error_panel.innerHTML = `<b>${response.error_message}</b>`
+				error_panel.style.display = "block";
+			}
+			return false;
+		}
+		password_error.style.display = "block";
+		return false;
+	}
+	username_error.style.display = "block";
+	return false;
+};
+
+module.exports = {
+	login: login
+};
