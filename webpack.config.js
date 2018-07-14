7621d25f99926462318929d710371ae9bb532b72
reiosantos <ronireiosantos@gmail.com>
2018-07-14 17:52:18 +0300

    create webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+	entry: ["babel-polyfill", "./static/js_src/entry"],
+	output: {
+		path: path.resolve(__dirname, "static/js"),
+		filename: "main.bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["env"]
+					}
+				}
+			}
+		]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: "source-map"
+};
