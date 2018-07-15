7c9ad30d0e5bf4a0b5c4fdfcf3e2ebd7e8be4b7d
reiosantos <ronireiosantos@gmail.com>
2018-07-15 23:03:23 +0300

    remove save ride and search ride methods

--- static/js_src/extras/main.js
+++ static/js_src/extras/main.js
@@ -109,31 +109,6 @@ let formatDateTime = (date) => {
 
 };
 
-function saveRide(form) {
-	const location = form.inputLocation;
-	const amount = form.inputCost;
-
-	document.getElementById("locationError").style.display = "none";
-	document.getElementById("costError").style.display = "none";
-
-	if (form.id === "form-new-offer") {
-
-		if (location.value.length > 3) {
-			if (validateAmount(amount.value)) {
-				return true;
-			}
-			document.getElementById("costError").style.display = "block";
-			return false;
-		}
-		document.getElementById("locationError").style.display = "block";
-		return false;
-	}
-	return false;
-}
-
-function searchRide(event) {
-	console.log(event);
-}
 export let exports = {};
 
-export {validateContact, validatePassword, validateAmount, saveRide, searchRide, validateTime, validateDate, formatDateTime};
+export {validateContact, validatePassword, validateAmount, validateTime, validateDate, formatDateTime};
