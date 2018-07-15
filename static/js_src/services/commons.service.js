c11f2a31b77734c4a30d10aafbb6baa69ed48989
reiosantos <ronireiosantos@gmail.com>
2018-07-15 20:02:17 +0300

    create single shared service

---
+++ static/js_src/services/commons.service.js
@@ -0,0 +1,53 @@
+
+import {HEADERS, LOGIN_URL, LOGOUT_URL, PROPERTY_AUTH_TOKEN, PROPERTY_USER} from "../extras/variable_constants";
+
+let make_request_service = (URL, data={}) => {
+
+	HEADERS["Authorization"] = `JWT ${localStorage.getItem(PROPERTY_AUTH_TOKEN)}`;
+
+	return fetch(URL,
+		{
+			method: "POST",
+			headers: HEADERS,
+			body: JSON.stringify(data)
+		})
+		.then(response => {
+			const regExp = /^2[0-9].*$/;
+
+			if (regExp.test(response.status)) {
+				return Promise.resolve(response);
+			}else {
+				return Promise.reject(response);
+			}
+		})
+		.then(response => response.json()) // parse response as JSON
+		.then(data => {
+			// success
+			if (URL === LOGOUT_URL) {
+				localStorage.clear();
+				return true;
+
+			}else if (URL === LOGIN_URL) {
+				if (data.hasOwnProperty(PROPERTY_AUTH_TOKEN)) {
+					localStorage.setItem(PROPERTY_AUTH_TOKEN, data[PROPERTY_AUTH_TOKEN]);
+				}
+				if (data.hasOwnProperty(PROPERTY_USER)) {
+					localStorage.setItem(PROPERTY_USER, JSON.stringify(data[PROPERTY_USER]));
+				}
+				return true;
+			}
+			return data;
+		})
+		.catch(function (error) {
+			if (URL === LOGIN_URL) {
+				return error.json();
+			}
+			return error;
+		});
+};
+
+module.exports = {
+	login_service: make_request_service,
+	logout_service: make_request_service,
+	signup_service: make_request_service
+};
