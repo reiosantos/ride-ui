eb7aca210568a958146b0700f83399e2c1112977
reiosantos <ronireiosantos@gmail.com>
2018-07-15 18:32:07 +0300

    add url path variables

--- static/js_src/variable_constants.js
+++ static/js_src/variable_constants.js
@@ -3,9 +3,17 @@ export const HEADERS = {
 	"Accept": "application/json",
 	"Content-type":"application/json"
 };
+export const LOGIN_PATH = "/templates/login.html";
+export const SIGNUP_PATH = "/templates/signup.html";
+export const WELCOME_PATH = "/index.html";
+export const PASSENGER_PATH = "/templates/passenger/index.html";
+export const DRIVER_PATH = "/templates/driver/index.html";
+
 export const BASE_URL = "http://localhost:5000/api/v1";
 
 export const LOGIN_URL = `${BASE_URL}/auth/login`;
+export const LOGOUT_URL = `${BASE_URL}/auth/logout`;
+export const SIGNUP_URL = `${BASE_URL}/auth/signup`;
 
 export const PROPERTY_AUTH_TOKEN = "auth_token";
 export const PROPERTY_USER = "user";
